6db791bc8dd2a7d3a56f1b07cc8f2b4ad46deead
xLifeFelix <xlifehd@gmail.com>
2014-11-06 22:57:04 +0100

    Upload

---
+++ testing.lua
@@ -0,0 +1,61 @@
+--[[
+ts3.getChannelOfClient(serverConnectionHandlerID, clientID) > channelID, error
+ts3.getCurrentServerConnectionHandlerID()
+ts3.printMessageToCurrentTab(message)
+ts3.requestClientPoke(serverConnectionHandlerID, clientID, message) > error
+ts3.getClientList(serverConnectionHandlerID) > clientsList, error
+
+local myChannelName, error = ts3.getChannelVariableAsString(serverConnectionHandlerID, myChannelID, ts3defs.ChannelProperties.CHANNEL_NAME)
+if error ~= ts3errors.ERROR_ok then
+	print("Error getting channel name: " .. error)
+	return
+end]]--
+
+require("ts3defs")      -- Required
+require("ts3errors")    -- Required
+require("ts3init")      -- Required
+require("ts3autoload")
+require("ts3events")
+
+function xprint(msg)
+	ts3.printMessageToCurrentTab(msg)
+end
+serverConnectionHandlerID = ts3.getCurrentServerConnectionHandlerID()
+myClientID = ts3.getClientID(serverConnectionHandlerID)
+
+function getIDs()
+	Clients = ts3.getClientList(serverConnectionHandlerID)
+	for i = 1, #Clients do
+		local Nickname, error = ts3.getClientVariableAsString(serverConnectionHandlerID, Clients[i], ts3defs.ClientProperties.CLIENT_NICKNAME)
+		if error ~= ts3errors.ERROR_ok then
+			xprint("Error getting client nickname: " .. error .. " | ID: " .. Clients[i])
+			return
+		end
+		xprint("ID: " .. Clients[i] .. " | Nickname: " .. Nickname)
+	end
+end
+
+function onTextMessageEvent(serverConnectionHandlerID, targetMode, toID, fromID, fromName, fromUniqueIdentifier, message, ffIgnored)
+	print("Lua: onTextMessageEvent: " .. serverConnectionHandlerID .. " " .. targetMode .. " " .. toID .. " " .. fromID .. " " .. fromName .. " " .. fromUniqueIdentifier .. " " .. message .. " " .. ffIgnored)
+	if string.lower(string.sub(message, 1, 4) == "info" then -- info add
+		if string.lower(string.sub(message, 6, 8) == "add" then --   1234567890
+			save(fromName, UniqueID, string.sub(message, 10, string.len(message))
+		elseif string.lower(string.sub(message, 6, 9) == "show" then
+			show(fromID, string.sub(message, 11, string.len(message))
+		end
+	end	
+end
+
+function save(Nickname, UniqueID, Line)
+
+end
+
+function show(fromID, Name)
+
+end
+
+
+
+
+
+
