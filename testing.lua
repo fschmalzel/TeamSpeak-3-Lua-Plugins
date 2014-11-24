04ef7937f747c7d412912a2e2d6d064545fc95e2
xLifeFelix <xlifehd@gmail.com>
2014-11-24 15:23:27 +0100

    Debug Code

--- testing.lua
+++ testing.lua
@@ -28,31 +28,39 @@ loadFile()				Loading the File 50%
 deleteClient()			Deleting an Client out of the table 100% Benachrichtigungen fehlen
 ]]
 
-local function xprint(msg)
+function xprint(msg)
 	ts3.printMessageToCurrentTab(msg)
 end
 
 function onTextMessageEvent(serverConnectionHandlerID, targetMode, toID, fromID, fromName, fromUniqueIdentifier, message, ffIgnored)
 	print("Lua: onTextMessageEvent: " .. serverConnectionHandlerID .. " " .. targetMode .. " " .. toID .. " " .. fromID .. " " .. fromName .. " " .. fromUniqueIdentifier .. " " .. message .. " " .. ffIgnored)
 	if string.lower(string.sub(message, 1, 4)) == "info" then --     info add
+		xprint("info")
 		if string.lower(string.sub(message, 6, 8)) == "add" then --   1234567890
+			xprint("add"..string.sub(message, 10, string.len(message)))
 			save(fromName, fromUniqueIdentifier, string.sub(message, 10, string.len(message)))
 		elseif string.lower(string.sub(message, 6, 9)) == "show" then
+			xprint("show")
 			show(fromID, string.sub(message, 11, string.len(message)))
 		elseif string.lower(string.sub(message, 6, 11)) == "update" then
+			xprint("update")
 			updateNick(fromID, fromName, fromUniqueIdentifier)
 		elseif string.lower(string.sub(message, 6, 12)) == "delline" then
+			xprint("delline")
 			deleteLine(fromID, fromUniqueIdentifier, string.sub(message, 14, string.len(message)))
 		elseif string.lower(string.sub(message, 6, 10)) == "delme" then
+			xprint("delme")
 			deleteClient(fromID, fromUniqueIdentifier, string.sub(message, 12, string.len(message)))
 		end
 	end	
 end
 
-local function save(Nickname, UniqueID, Line)
+function save(Nickname, UniqueID, Line)
+	xprint("HEY")
 	local X = 0
 	local exists = false
-	for index, value in pairs(data) do
+	for index, value in ipairs(data) do
+		xprint(index.." | "..tostring(value))
 		if value[2] == UniqueID then
 			exists = true
 			X = index
@@ -61,13 +69,16 @@ local function save(Nickname, UniqueID, Line)
 	if exists == true then
 		data[X][3][#data[X][3]+1] = Line -- Adding one line
 		data[X][1] = Nickname -- Changing the Nickname
+		xprint(data[X][1]..data[X][2]..data[X][3][1])--Debug
 		updateFile()
 	else
 		data[#data+1]={Nickname, UniqueID, {Line}} -- Creating a new User
+		X = #data --Debug
+		xprint(data[X][1]..data[X][2]..data[X][3][1])--Debug
 	end
 end
 
-local function show(ClientID, Nickname)
+function show(ClientID, Nickname)
 	local X = 0
 	local exists = false
 	for index, value in ipairs(data) do
@@ -95,15 +106,15 @@ local function show(ClientID, Nickname)
 	end
 end
 
-local function updateFile()
+function updateFile()
 	
 end
 --table structure: data{{Nickname, UniqueID, {Line1, Line2, Line3, ...}},{Nickname, UniqueID, {Line1, Line2, Line3, ...}}
 --data.dat
 --L1:N:"Nickname",U:"UniqueID",L:{1:"Line1",2:"Line2",3:"Line3"}
 --L9:
-local function loadFile()
-	for --datei laden lines
+function loadFile()
+--[[	for --datei laden lines
 		data[i]={"Nickname", "UniqueID", {"Line"}}
 		Pos1, x = string.find(Line, 'N:"')
 		x, Pos2 = string.find(Line, '",')
@@ -118,10 +129,10 @@ local function loadFile()
 			data[i][2] = UniqueID
 		end
 		--Linien mussen noch geldaden werden
-	end
+	end]]
 end
 
-local function updateNick(ClientID, Nickname, UniqueID)
+function updateNick(ClientID, Nickname, UniqueID)
 	local X = 0
 	local exists = false
 	for index, value in pairs(data) do
@@ -141,7 +152,7 @@ local function updateNick(ClientID, Nickname, UniqueID)
 	end
 end
 
-local function deleteLine(ClientID, UniqueID, numberLine)
+function deleteLine(ClientID, UniqueID, numberLine)
 	local X = 0
 	local exists = false
 	for index, value in pairs(data) do
@@ -166,7 +177,7 @@ local function deleteLine(ClientID, UniqueID, numberLine)
 	end
 end
 
-local function deleteClient(ClientID, UniqueID, Msg)
+function deleteClient(ClientID, UniqueID, Msg)
 	if string.lower(Msg) == "yes" then
 		local X = 0
 		local exists = false
