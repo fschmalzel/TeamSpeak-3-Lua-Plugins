38d18d91163c914ba2ab42fd8850cd6df3c182a2
xLifeFelix <xlifehd@gmail.com>
2016-05-05 02:07:06 +0200

    Fixed AP CI & added some Formatting

--- Admin Pack/plugins/lua_plugin/xlife_Clientinfo/main.lua
+++ Admin Pack/plugins/lua_plugin/xlife_Clientinfo/main.lua
@@ -1,7 +1,7 @@
 require("ts3defs")
 require("ts3errors")
 
-function xprint(msg)
+local function xprint(msg)
 	ts3.printMessageToCurrentTab(msg)
 end
 xprint("xLife ClientInfo wird geladen.")
@@ -16,46 +16,55 @@ end
 
 clinfohelp()
 
-function clinfo(serverConnectionHandlerID, adv)
-	Clients, error = ts3.getClientList(serverConnectionHandlerID)
+local function formatString(inputstring, digits, character)
+	return string.rep(character, digits - string.len(tostring(inputstring))) .. tostring(inputstring)
+end
+
+function clinfo(serverConnectionHandlerID, ...)
+	local arg = { ... }
+	local adv = ""
+	if type(arg[1]) ~= "nil" and string.lower(tostring(arg[1])) == "true" then
+		adv = true
+	else 
+		adv = false
+	end
+	local Clients, error = ts3.getClientList(serverConnectionHandlerID)
 	if error ~= ts3errors.ERROR_ok then xprint("Error getting list of clients: " .. error .. " | SID: " .. serverConnectionHandlerID) end
 	xprint("┌───────────────────────────────────────────────────────────────────────────────────────────────")
-	if adv == "true" then
-		xprint("│ ClID | DBID | UniqueID | Type | Nickname | Description | Servergroups")
-	else
-		xprint("│ ClID | DBID | UniqueID | Nickname")
-	end
 	for i, clientID in ipairs(Clients) do
-		local ClientID = (string.rep("0", 4 - string.len(clientID)) .. clientID)
-		local ClientDBID, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, 32)
-		if error ~= ts3errors.ERROR_ok then xprint("Error getting ClientDBID: " .. error .. " | SID: " .. serverConnectionHandlerID .. " | ClientID: " .. clientID) end
-		if string.len(ClientDBID) < 4 then
-			ClientDBID = (string.rep("0", 4 - string.len(ClientDBID)) .. ClientDBID)
-		end
-		local ClientUID, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, 0)
-		if error ~= ts3errors.ERROR_ok then xprint("Error getting ClientUID: " .. error .. " | SID: " .. serverConnectionHandlerID .. " | ClientID: " .. clientID) end
-		local ClientNickname, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, 1)
-		if error ~= ts3errors.ERROR_ok then xprint("Error getting ClientNickname: " .. error .. " | SID: " .. serverConnectionHandlerID .. " | ClientID: " .. clientID) end
-		if adv == "true" then
-			ClientType, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, 40)
-			if error ~= ts3errors.ERROR_ok then xprint("Error getting ClientType: " .. error .. " | SID: " .. serverConnectionHandlerID .. " | ClientID: " .. clientID) end
-			local ClientSrvGrps, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, 34)
-			if error ~= ts3errors.ERROR_ok then xprint("Error getting ClientSrvGrps: " .. error .. " | SID: " .. serverConnectionHandlerID .. " | ClientID: " .. clientID) end
-			local ClientDscrp, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, 45)
-			if error ~= ts3errors.ERROR_ok then xprint("Error getting Client Description: " .. error .. " | SID: " .. serverConnectionHandlerID .. " | ClientID: " .. clientID) end
-			xprint("│ " .. ClientID .. " | " .. ClientDBID .. " | " .. ClientUID .. " | " .. ClientType .. " | " .. ClientNickname .. " | " .. ClientDscrp .. " | " .. ClientSrvGrps)
-		else
-			xprint("│ " .. ClientID .. " | " .. ClientDBID .. " | " .. ClientUID .. " | " .. ClientNickname)
-		end
-		if (math.floor(i / 3)) == (i / 3) and #Clients ~= i then
+		if (i % 3) == 0 and #Clients ~= i then
 			xprint("├───────────────────────────────────────────────────────────────────────────────────────────────")
 		end
-		if (math.floor(i / 9)) == (i / 9) and #Clients ~= i then
-			if adv == "true" then
-				xprint("│ ClID | DBID | UniqueID | Type | Nickname | Description | Servergroups")
+		if (i % 9) == 0 and #Clients ~= i or i == 1 then
+			if adv == true then
+				xprint("│ CliID |#DBID#|########UniqueID########| Type | Nickname | Description | Servergroups")
 			else
 				xprint("│ ClID | DBID | UniqueID | Nickname")
 			end
+		end
+		
+		local ClientDBID, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, 32)
+		if error ~= ts3errors.ERROR_ok then xprint("Error getting ClientDBID: " .. error .. " | SID: " .. serverConnectionHandlerID .. " | ClientID: " .. clientID) end
+		
+		local ClientUID, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, 0)
+		if error ~= ts3errors.ERROR_ok then xprint("Error getting ClientUID: " .. error .. " | SID: " .. serverConnectionHandlerID .. " | ClientID: " .. clientID) end
+		
+		local ClientNickname, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, 1)
+		if error ~= ts3errors.ERROR_ok then xprint("Error getting ClientNickname: " .. error .. " | SID: " .. serverConnectionHandlerID .. " | ClientID: " .. clientID) end
+		
+		if adv == true then
+			local ClientType, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, 40)
+			if error ~= ts3errors.ERROR_ok then xprint("Error getting ClientType: " .. error .. " | SID: " .. serverConnectionHandlerID .. " | ClientID: " .. clientID) end
+			
+			local ClientSrvGrps, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, 34)
+			if error ~= ts3errors.ERROR_ok then xprint("Error getting ClientSrvGrps: " .. error .. " | SID: " .. serverConnectionHandlerID .. " | ClientID: " .. clientID) end
+			
+			local ClientDscrp, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, 45)
+			if error ~= ts3errors.ERROR_ok then xprint("Error getting Client Description: " .. error .. " | SID: " .. serverConnectionHandlerID .. " | ClientID: " .. clientID) end
+			
+			xprint("│ " .. formatString(clientID, 4, "0") .. " | " .. formatString(ClientDBID, 5, "0") .. " | " .. formatString(ClientUID, 28, " ") .. " | " .. ClientType .. " | \"" .. ClientNickname .. "\" | \"" .. ClientDscrp .. "\" | " .. ClientSrvGrps)
+		else
+			xprint("│ " .. formatString(clientID, 4, "0") .. " | " .. formatString(ClientDBID, 5, "0") .. " | " .. formatString(ClientUID, 28, " ") .. " | " .. ClientNickname)
 		end
 	end
 	xprint("└───────────────────────────────────────────────────────────────────────────────────────────────")
