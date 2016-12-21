d0995ac29697dd0c74ae480d82cb10e3387442c3
xLifeFelix <xlifehd@gmail.com>
2016-12-21 18:52:05 +0100

    Moved xlife_mp

--- Massmover Masspoke (Moved to Admin Pack)/plugins/lua_plugin/xlife_mp/init.lua
+++
@@ -1,8 +0,0 @@
-require("ts3init")            -- Required for ts3RegisterModule
-require("xlife_mp/main")    -- Some demo functions callable from TS3 client chat input
-
-local MODULE_NAME = "xlife_mp"
-
-local registeredEvents = {}
-
-ts3RegisterModule(MODULE_NAME, registeredEvents)
